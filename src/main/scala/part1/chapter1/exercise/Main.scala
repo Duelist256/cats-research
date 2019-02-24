e6654833a1ad7a463be255dcf3fae2821171c601
Duelist256 <selykov.iw@yandex.ru>
2019-02-24 16:09:08 +0300

    Solve exercise with Printable

---
+++ src/main/scala/part1/chapter1/exercise/Main.scala
@@ -0,0 +1,16 @@
+package part1.chapter1.exercise
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    import PrintableInstances._
+
+    // Interface object
+    val cat = Cat("Garfield", 38, "ginger and black")
+    Printable.print(cat)
+
+    // Interface syntax
+    import PrintableSyntax._
+    val cat2 = Cat("Tom", 54, "blue")
+    cat2.print
+  }
+}
---
+++ src/main/scala/part1/chapter1/exercise/PrintableInstances.scala
@@ -0,0 +1,20 @@
+package part1.chapter1.exercise
+
+object PrintableInstances {
+  implicit val stringPrintable: Printable[String] = new Printable[String] {
+    override def format(value: String): String = value
+  }
+
+  implicit val intPrintable: Printable[Int] = new Printable[Int] {
+    override def format(value: Int): String = value.toString
+  }
+
+  implicit val catPrintable: Printable[Cat] = new Printable[Cat] {
+    override def format(cat: Cat): String = {
+      val name = Printable.format(cat.name)
+      val age = Printable.format(cat.age)
+      val color = Printable.format(cat.color)
+      s"$name is a $age years old $color cat"
+    }
+  }
+}
